d44ba0e71eca54f7fa1c404446de51c6e2f27d85
mickael <nv.osselaer@gmail.com>
2021-03-16 16:18:39 +0100

    CREATION de la page salle de bain and merge

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="CSS/style.css">
-    <link rel="stylesheet" href="CSS/style_tablette.css">
+    <link rel="stylesheet" href="CSS_TABLETTE/style_tablette.css">
     <script src="https://kit.fontawesome.com/0fa7bf339b.js" crossorigin="anonymous"></script>
     <title>Fac'eco</title>
 </head>
