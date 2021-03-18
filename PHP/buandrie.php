84c5e306c07516f8f1c4695f3d28409700ce871f
mickael <nv.osselaer@gmail.com>
2021-03-18 08:56:07 +0100

    fichier PHP

---
+++ PHP/buandrie.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../CSS/buandrie.css">
+    <title>Buandrie</title>
+</head>
+<body>
+    <div id="title">
+        <img src="../IMG/logo_ecobleu.png/" alt="logo_ecobleu" class="logo_ecobleu">
+        <h1>éco-gestes au quotidien</h1>
+    </div>
+    <div id="container_title">
+        <h2>Buandrie</h2>
+    </div>
+    <div id="image">
+        <img src="../IMG/Buanderie.png/" alt="buandrie" class="buandrie">
+    </div>
+    <div id="container_temperature">
+        <div id="container_title_temp">
+            <h2 class="title_temp">Agir sur la température</h2>
+        </div>
+        <div id="text_one">
+            <p>Dès que la température est plus douce baissez votre chauffage</p>
+        </div>
+        <div id="text_two">
+            <p>Entretenez votre chaudière 1 fois par an</p>
+        </div>
+        <div id="text_three">
+            <p>Régler la température du chauffe-eau entre 55°C à 60°C</p>
+        </div>
+        <div id="text_four">
+            <p>Si vous partez plus d’une journée, mettez le chauffage en mode hors-gel et coupez le ballon d’eau chaude électrique</p>
+        </div>
+    </div>
+
+</body>
+</html>
