0ea70baaa0c03b536ffbc86cce0be6a61e1fb459
mickael <nv.osselaer@gmail.com>
2021-03-18 13:36:49 +0100

    COMMIT 4.0

--- PHP/chambre_agir_sur_la_temperature.php
+++ PHP/chambre_agir_sur_la_temperature.php
@@ -11,44 +11,44 @@
     <title>Fac'eco-chambre-agir-sur-la-temperature</title>
 </head>
 <body>
-        <div id="title">
-            <img src="../IMG/logo_ecobleu.png/" alt="logo_ecobleu" class="logo_ecobleu">
-            <h1>éco-gestes au quotidien</h1>
+    <div id="title">
+        <img src="../IMG/logo_ecobleu.png/" alt="logo_ecobleu" class="logo_ecobleu">
+        <h1>éco-gestes au quotidien</h1>
+    </div>
+    <div id="container_principal">
+        <div id="container_title">
+            <h2>Chambre</h2>
         </div>
-        <div id="container_principal">
-            <div id="container_title">
-                <h2>Chambre</h2>
+        <div id="image">
+            <img src="../IMG/Chambre.png/" alt="chambre" class="chambre">
+        </div>
+        <div id="container_temperature">
+            <div id="container_title_temp">
+                <h2 class="title_temp">Agir sur la température</h2>
             </div>
-            <div id="image">
-                <img src="../IMG/Chambre.png/" alt="chambre" class="chambre">
+            <div id="text_one">
+                <p>16 à 18° température recommandée la nuit</p>
+                <span class="italic_temp">Baisser le chauffage de 1°C = -7% d'énergie consommée</span>
             </div>
-            <div id="container_temperature">
-                <div id="container_title_temp">
-                    <h2 class="title_temp">Agir sur la température</h2>
-                </div>
-                <div id="text_one">
-                    <p>16 à 18° température recommandée la nuit</p>
-                    <span class="italic_temp">Baisser le chauffage de 1°C = -7% d'énergie consommée</span>
-                </div>
-                <div id="text_two">
-                    <p>Eviter de faire pendre vos rideaux devant les radiateurs</p>
-                </div>
-                <div id="text_three">
-                    <p>Purger régulièrement les radiateurs</p>
-                </div>
-                <div id="text_four">
-                    <p>Fermer les volets la nuit</p>
-                </div>
-                <div id="text_five">
-                    <p>Installer des rideaux épais et fermez-les la nuit</p>
-                </div>
+            <div id="text_two">
+                <p>Eviter de faire pendre vos rideaux devant les radiateurs</p>
             </div>
-            <div id="retour">
-                <a href="../index.php">
-                    <i class="fas fa-undo"></i>
-                </a>
-                <h3>Retour menu</h3>
+            <div id="text_three">
+                <p>Purger régulièrement les radiateurs</p>
+            </div>
+            <div id="text_four">
+                <p>Fermer les volets la nuit</p>
+            </div>
+            <div id="text_five">
+                <p>Installer des rideaux épais et fermez-les la nuit</p>
             </div>
         </div>
+        <div id="retour">
+            <a href="../index.php">
+                <i class="fas fa-undo"></i>
+            </a>
+            <h3>Retour menu</h3>
+        </div>
+    </div>
 </body>
 </html>
