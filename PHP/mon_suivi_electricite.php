3d34883061b729d18df256f69a57746da92e300d
mickael <nv.osselaer@gmail.com>
2021-03-19 13:59:01 +0100

    image

---
+++ PHP/mon_suivi_electricite.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../CSS/mon_suivi_electricite.css">
+    <title>Mon-suivi-electricite</title>
+</head>
+<body>
+    <div class="my_follow_up">
+        <img src="../IMG/logo_mon_suivi.png" alt="eclair" class="eclair">
+        <h1>Mon suivi</h1>
+    </div>
+    <canvas id="myChart" width="400" height="400"></canvas>
+
+    <canvas id="myGraphic" width="400" height="400"></canvas>
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+    <script src="../JS/graphic.js"></script>
+</body>
+</html>
