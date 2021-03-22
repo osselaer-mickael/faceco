b3d3b98aa3c4b60c978e18e8a5cf80b0b2d4e3ac
mickael <nv.osselaer@gmail.com>
2021-03-22 10:33:28 +0100

    image

---
+++ PHP/les_consignes_de_tri.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://kit.fontawesome.com/0fa7bf339b.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../CSS/les_consignes_de_tri.css">
+    <title>Document</title>
+</head>
+<body>
+    <div id="title">
+        <img src="../IMG/logo_ecobleu.png/" alt="logo_tri_selectif" class="logo_tri_selectif">
+        <h1>Tri-sélectif</h1>
+    </div>
+
+</body>
+</html>
