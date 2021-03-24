929a224bc5d398088902bf2b849af8a6e41e912f
mickael <nv.osselaer@gmail.com>
2021-03-24 13:46:17 +0100

    responsive "eco-geste"

--- PHP/cuisine.php
+++ PHP/cuisine.php
@@ -20,7 +20,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://kit.fontawesome.com/0fa7bf339b.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../CSS/cuisine.css">
-    <link rel="stylesheet" href="../CSS_TABLETTE/cuisine_tablette.css">
     <title>Cuisine</title>
 </head>
 <body>
@@ -28,62 +27,66 @@
         <img src="../IMG/logo_ecobleu.png/" alt="logo_ecobleu" class="logo_ecobleu">
         <h1>Eco-gestes au quotidien</h1>
     </div>
-    <div id="image">
-        <img src="../IMG/Cuisine.png/" alt="cuisine" class="cuisine">
-    </div>
-    <div id="container_title_elec">
-        <a href="./cuisine.php?container_elec">
-            <h2 class="title_elec">Agir sur l'électricité</h2>
-        </a>
-    </div>
-    <div id="electricite" <?= $container_electricite ?>>
-        <div id="text_one_elec">
-            <p>Nettoyer régulièrement la grille arrière du réfrigérateur</p>
+    <div id="responsive">
+        <div id="image">
+            <img src="../IMG/Cuisine.png/" alt="cuisine" class="cuisine">
         </div>
-        <div id="text_two_elec">
-            <p>Nettoyer régulièrement le filtre de votre lave-vaisselle</p>
-        </div>
-        <div id="text_three_elec">
-            <p>Dégivrer régulièrement le congélateur et le réfrigérateur</p>
-        </div>
-        <div id="text_four_elec">
-            <p>Remplissez complètement votre lave-vaisselle</p>
-        </div>
-        <div id="text_five_elec">
-            <p>Laisser refroidir les plats avant de les mettre au réfrigérateur</p>
-        </div>
-        <div id="text_six_elec">
-            <p>Utiliser des casseroles adaptées à la taille des plaques</p>
-        </div>
-        <div id="text_seven_elec">
-            <p>Pendant la cuisson, couvrez les casseroles</p>
-            <span class="italic_ardoise">= 4x moins d’électricité ou de gaz consommés</span>
-        </div>
-        <div id="text_eight_elec">
-            <p>Coupez les plaques électriques un peu avant la fin de la cuisson</p>
-        </div>
-        <div id="text_nine_elec">
-            <p>J’ouvre le congélateur et le réfrigérateur le moins longtemps possible pour ne pas laisser le froid s’échapper</p>
-        </div>
-        <div id="text_ten_elec">
-            <p>J’évite de mettre des plats encore chaud au réfrigérateur</p>
-        </div>
-        <div id="text_eleven_elec">
-            <p>Je choisis des appareils électroménagers économes, classés de A+ à A+++</p>
-            <span class="italic_ardoise">Un appareil A+++ consomme jusqu’à 50% de moins</span>
-        </div>
-    </div>
-    <div id="container_title_eau">
-        <a href="./cuisine.php?container_eau">
-            <h2 class="title_eau">Agir sur l'eau</h2>
-        </a>
-    </div>
-    <div id="eau"  <?= $container_eau ?>>
-        <div id="text_one_eau">
-            <p>Ne prélavez pas la vaisselle</p>
-        </div>
-        <div id="text_two_eau">
-            <p>Utilisez 2 bacs pour faire la vaisselle afin d’éviter de laisser couler l’eau</p>
+        <div id="container-principal">
+            <div id="container_title_elec">
+                <a href="./cuisine.php?container_elec">
+                    <h2 class="title_elec">Agir sur l'électricité</h2>
+                </a>
+            </div>
+            <div id="electricite" <?= $container_electricite ?>>
+                <div id="text_one_elec">
+                    <p>Nettoyer régulièrement la grille arrière du réfrigérateur</p>
+                </div>
+                <div id="text_two_elec">
+                    <p>Nettoyer régulièrement le filtre de votre lave-vaisselle</p>
+                </div>
+                <div id="text_three_elec">
+                    <p>Dégivrer régulièrement le congélateur et le réfrigérateur</p>
+                </div>
+                <div id="text_four_elec">
+                    <p>Remplissez complètement votre lave-vaisselle</p>
+                </div>
+                <div id="text_five_elec">
+                    <p>Laisser refroidir les plats avant de les mettre au réfrigérateur</p>
+                </div>
+                <div id="text_six_elec">
+                    <p>Utiliser des casseroles adaptées à la taille des plaques</p>
+                </div>
+                <div id="text_seven_elec">
+                    <p>Pendant la cuisson, couvrez les casseroles</p>
+                    <span class="italic_ardoise">= 4x moins d’électricité ou de gaz consommés</span>
+                </div>
+                <div id="text_eight_elec">
+                    <p>Coupez les plaques électriques un peu avant la fin de la cuisson</p>
+                </div>
+                <div id="text_nine_elec">
+                    <p>J’ouvre le congélateur et le réfrigérateur le moins longtemps possible pour ne pas laisser le froid s’échapper</p>
+                </div>
+                <div id="text_ten_elec">
+                    <p>J’évite de mettre des plats encore chaud au réfrigérateur</p>
+                </div>
+                <div id="text_eleven_elec">
+                    <p>Je choisis des appareils électroménagers économes, classés de A+ à A+++</p>
+                    <span class="italic_ardoise">Un appareil A+++ consomme jusqu’à 50% de moins</span>
+                </div>
+            </div>
+            <div id="container_title_eau">
+                <a href="./cuisine.php?container_eau">
+                    <h2 class="title_eau">Agir sur l'eau</h2>
+                </a>
+            </div>
+            <div id="eau"  <?= $container_eau ?>>
+                <div id="text_one_eau">
+                    <p>Ne prélavez pas la vaisselle</p>
+                </div>
+                <div id="text_two_eau">
+                    <p>Utilisez 2 bacs pour faire la vaisselle afin d’éviter de laisser couler l’eau</p>
+                </div>
+            </div>
         </div>
     </div>
     <div id="retour">
--- PHP/salon.php
+++ PHP/salon.php
@@ -19,7 +19,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://kit.fontawesome.com/0fa7bf339b.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../CSS/salon.css">
-    <link rel="stylesheet" href="../CSS_TABLETTE/salon_tablette.css">
     <title>Salon</title>
 </head>
 <body>
@@ -28,51 +27,55 @@
         <img src="../IMG/logo_ecobleu.png/" alt="logo_ecobleu" class="logo_ecobleu">
         <h1>Eco-gestes au quotidien</h1>
     </div>
-    <div id="image">
-        <img src="../IMG/Salon.png/" alt="salon" class="salon">
-    </div>
-    <div id="container_title_elec">
-        <a href="./salon.php?container_elec">
-            <h2 class="title_elec">Agir sur l'électricité</h2>
-        </a>
-    </div>
-    <div id="electricite" <?= $container_elec ?>>
-        <div id="text_one_elec">
-            <p>Éteindre les lumières en sortant d’une pièce</p>
-            <span class="italic_ardoise">L’éclairage représente 10% de la consommation d'électricité en France</span>
+    <div id="responsive">
+        <div id="image">
+            <img src="../IMG/Salon.png/" alt="salon" class="salon">
         </div>
-        <div id="text_two_elec">
-            <p>Eviter l’usage de radiateurs d’appoint électrique ou gaz</p>
-        </div>
-        <div id="text_three_elec">
-            <p>Eteindre les veilles des appareils audiovisuels grâce à une multiprise</p>
-            <span class="italic_ardoise">Jusqu’à 10% d’économie d’énergie</span>
-        </div>
-    </div>
-    <div id="container_title_air">
-        <a href="./salon.php?container_air">
-            <h2 class="title_air">Agir sur la qualité de l’air</h2>
-        </a>
-    </div>
-    <div id="air" <?= $container_air ?>>
-        <div id="text_one_air">
-            <p>Aérer les pièces 5 à 10 min par jour</p>
-        </div>
-        <div id="text_two_air">
-            <p>Ne brûlez pas de bougies ou d’encens. Ils émettent des polluants</p>
-        </div>
-        <div id="text_three_air">
-            <p>Nettoyez et dépoussiérez les bouches de ventilation</p>
-        </div>
-        <div id="text_four_air">
-            <p>Débranchez votre ordinateur et/ou tablette. Même éteint ils consomment de l'électricité</p>
-        </div>
-        <div id="text_five_air">
-            <p>Evitez les abat-jour sombres ou épais</p>
-        </div>
-        <div id="text_six_air">
-            <p>Utilisez des lampes à LED</p>
-            <span class="italic_vert">Elles consomment 5 à 10 fois moins que les vieilles ampoules et durent 20 à 50 fois plus longtemps</span>
+        <div id="container-principal">
+            <div id="container_title_elec">
+                <a href="./salon.php?container_elec">
+                    <h2 class="title_elec">Agir sur l'électricité</h2>
+                </a>
+            </div>
+            <div id="electricite" <?= $container_elec ?>>
+                <div id="text_one_elec">
+                    <p>Éteindre les lumières en sortant d’une pièce</p>
+                    <span class="italic_ardoise">L’éclairage représente 10% de la consommation d'électricité en France</span>
+                </div>
+                <div id="text_two_elec">
+                    <p>Eviter l’usage de radiateurs d’appoint électrique ou gaz</p>
+                </div>
+                <div id="text_three_elec">
+                    <p>Eteindre les veilles des appareils audiovisuels grâce à une multiprise</p>
+                    <span class="italic_ardoise">Jusqu’à 10% d’économie d’énergie</span>
+                </div>
+            </div>
+            <div id="container_title_air">
+                <a href="./salon.php?container_air">
+                    <h2 class="title_air">Agir sur la qualité de l’air</h2>
+                </a>
+            </div>
+            <div id="air" <?= $container_air ?>>
+                <div id="text_one_air">
+                    <p>Aérer les pièces 5 à 10 min par jour</p>
+                </div>
+                <div id="text_two_air">
+                    <p>Ne brûlez pas de bougies ou d’encens. Ils émettent des polluants</p>
+                </div>
+                <div id="text_three_air">
+                    <p>Nettoyez et dépoussiérez les bouches de ventilation</p>
+                </div>
+                <div id="text_four_air">
+                    <p>Débranchez votre ordinateur et/ou tablette. Même éteint ils consomment de l'électricité</p>
+                </div>
+                <div id="text_five_air">
+                    <p>Evitez les abat-jour sombres ou épais</p>
+                </div>
+                <div id="text_six_air">
+                    <p>Utilisez des lampes à LED</p>
+                    <span class="italic_vert">Elles consomment 5 à 10 fois moins que les vieilles ampoules et durent 20 à 50 fois plus longtemps</span>
+                </div>
+            </div>
         </div>
     </div>
     <div id="retour">
